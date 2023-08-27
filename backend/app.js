27cdc17667a9500bc4550a3b2379639ab6a6803d
huzaifa7ish <130061507+huzaifa7ish@users.noreply.github.com>
2023-08-27 16:41:20 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: 'http://',
+  origin: 'https://e-shop-livid-ten.vercel.app/',
   methods : ["POST" , "GET"]
   credentials: true
 }));
