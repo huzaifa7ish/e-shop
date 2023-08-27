d225bf4c1422429a440c2dff957757ac507e11cd
huzaifa7ish <130061507+huzaifa7ish@users.noreply.github.com>
2023-08-27 16:42:55 +0500

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://e-shop-livid-ten.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://e-shop-livid-ten.vercel.app/";
 
 
